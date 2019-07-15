f6442e761a57be5249c411727c6d45a7c47e9f84
prince <alsoprince@gmail.com>
2019-07-15 22:36:57 +0530

    Add New Fields In Profile Model

--- users/models.py
+++ users/models.py
@@ -17,6 +17,15 @@ class Profession(models.Model):
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name = "profile", null=True, blank=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.PositiveIntegerField()
+    university = models.CharField(max_length=100)
+    course = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    phone_number = models.PositiveIntegerField()
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
     profession = models.ForeignKey(Profession, on_delete=models.CASCADE, related_name="profiles", null=True, blank=True)
     
--- users/views.py
+++ users/views.py
@@ -7,15 +7,27 @@ from .models import Profession
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
+        u_form = UserUpdateForm(request.POST)
+        p_form = ProfileUpdateForm(request.POST, request.FILES)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
             messages.success(request, f'Your account has been created! You are now able to login')
             return redirect('login')
     else:
-        form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+        u_form = UserUpdateForm()
+        p_form = ProfileUpdateForm()
+
+    #     form = UserRegisterForm(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         username = form.cleaned_data.get('username')
+    #         messages.success(request, f'Your account has been created! You are now able to login')
+    #         return redirect('login')
+    # else:
+    #     form = UserRegisterForm()
+    return render(request, 'users/register.html', {'u_form': u_form, 'p_form': p_form})
+
 
 @login_required
 def profile(request):
